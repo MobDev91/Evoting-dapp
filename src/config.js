47772f4dcb835424b17a4d09f13d546cb517e14b
oussama.benyssef <oussama.benyssef@gmail.com>
2025-04-03 19:36:18 +0000

    Resolve Reset operation not working proprely

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const contractAddress = "0x168919F0AdC9b57C01575c13a0e436E70278CCC7";
+export const contractAddress = "0x7D2395A957A67f3fBb01Cca1A16135C23d4D2118";
 export const adminAddress = "0x1F603511f7D8d54b91439b920fC1f37fEA22e5f4";
 
